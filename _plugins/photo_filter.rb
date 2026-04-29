19279171f5f89c25ceb33460aaf7330f955a8ff6
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2026-04-29 13:36:20 +1200

    Normalize photo sort timestamps

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -1,4 +1,5 @@
 require "exiftool"
+require "time"
 
 module Jekyll
   module PhotoFilter
@@ -7,17 +8,28 @@ module Jekyll
     def photo_filter(files)
       photos = files.select { |photo| photo_file?(photo) }
       sorted = photos.sort_by { |photo|
-        begin
-          exif = Exiftool.new(photo.path)
-          exif[:date_time_original] || photo.modified_time
-        rescue StandardError
-          photo.modified_time
-        end
+        photo_time(photo)
       }
       sorted.reverse
     end
 
     private
+
+    def photo_time(photo)
+      exif = Exiftool.new(photo.path)
+      parse_time(exif[:date_time_original]) || photo.modified_time
+    rescue StandardError
+      photo.modified_time
+    end
+
+    def parse_time(value)
+      return value if value.is_a?(Time)
+      return nil if value.nil?
+
+      Time.parse(value.to_s.tr(":", "-", 2))
+    rescue ArgumentError
+      nil
+    end
 
     def photo_file?(file)
       relative_path = file.relative_path.tr("\\", "/").delete_prefix("./").delete_prefix("/")
